81948c7fdc43edbc78abc94beb7890f4b888c2eb
Taras <taras.v.vasylyshyn@gmail.com>
2023-06-18 14:36:11 +0300

    Implement create superuser feature.

--- app/core/models.py
+++ app/core/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
-)    
+)
 
 
 class UserManager(BaseUserManager):
@@ -18,6 +18,15 @@ class UserManager(BaseUserManager):
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
