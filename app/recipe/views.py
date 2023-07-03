6933da5cddf82ef8f84818b3831638e53ede7ed5
Taras <taras.v.vasylyshyn@gmail.com>
2023-07-03 14:48:29 +0300

    Added test for recipe detail.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -18,4 +18,4 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-id')    
+        return self.queryset.filter(user=self.request.user).order_by('-id')
